24a93aab827457788a99bede1c692474afcb95d7
nghiapt15scvaladdin <nghiapt15@fsoft.com.vn>
2022-06-18 22:31:11 +0700

    first init

---
+++ nghia/src/main/java/com/example/democmctraining/DemoCmcTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.democmctraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCmcTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCmcTrainingApplication.class, args);
+	}
+
+}
---
+++ nghia/src/test/java/com/example/democmctraining/DemoCmcTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democmctraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCmcTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
